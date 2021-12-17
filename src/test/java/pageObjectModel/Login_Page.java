8c967401e6e31dc2e78e31508f6a63075779dfdc
Rajasekar-Sampath <rajadhanush80@gmail.com>
2021-12-17 16:43:43 +0530

    First Commit

---
+++ src/test/java/pageObjectModel/Login_Page.java
@@ -0,0 +1,18 @@
+package pageObjectModel;
+
+public class Login_Page {
+	
+	public static void main (String[] args) {
+	
+	String name="Rajasekar";
+	
+	if(name == "Rajasekar") {
+		
+		System.out.println("it is correct");
+	} else {
+		
+		System.out.println("it is wrong");
+	}
+
+}
+}
